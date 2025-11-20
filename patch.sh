e44ac2306749fca94c142c7f62759612f24faf56
shaurizard <shaunsingh0207@gmail.com>
2025-11-20 12:08:54 -0500

    add libdovi

--- patch.sh
+++ patch.sh
@@ -51,8 +51,3 @@ fi
 for filename in $BASEDIR/patches/*.patch; do
     patch -t -N -p1 -d $src_dir < "$filename"  || exit 1
 done
-
-# The flatpak build refers to the latest commit, so we add a commit that includes the patches
-cd $src_dir
-git add .
-git -c user.name='deadbeef' -c user.email='deadbeef' commit -m "Patch"
